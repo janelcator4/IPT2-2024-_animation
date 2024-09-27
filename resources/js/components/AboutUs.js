36701e26686c86513750886d59af581e935e7925
catorjalen <cator.jalen@gmail.com>
2024-09-27 13:25:36 +0800

    first commit

---
+++ resources/js/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function AboutUs() {
+    return (
+        <div className='home'>
+            <Link to="/">Home</Link> <br />
+            <Link to="/aboutus">About Us</Link> <br />
+            <Link to="/contactus">Contact Us</Link> <br />
+            <hi>Home</hi>
+        </div>
+    )
+}
